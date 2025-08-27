e7a8d547c124d54f37cca918bb756f5a1ef03840
Rico Groth <rico.groth@check24.de>
2025-08-27 17:54:13 +0200

    trying some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+use std::{error::Error, io::Error, path::PathBuf};
+
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
@@ -18,27 +21,33 @@ enum Commands {
     },
 }
 
-fn init(zk_directory: &Option<std::path::PathBuf>) {
-    let zk_working_dir_relative = match zk_directory {
-        Some(directory) => {
-            let _ = std::fs::create_dir(directory);
-            directory.clone()
-        }
-        None => match std::env::current_dir() {
-            Ok(path) => path,
-            Err(_) => panic!("Cannot find zk directory"),
-        },
-    };
-    let zk_working_dir_absolute = match std::fs::canonicalize(&zk_working_dir_relative) {
-        Ok(path) => path,
-        Err(_) => panic!("Cannot find zk directory"),
-    };
-    println!("{:?}", zk_working_dir_absolute)
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    test: i32,
+}
+
+fn init_zk_working_dir(path: PathBuf) -> Result<(), Box<dyn Error>> {
+    let zk_conf_path = path.join(PathBuf::from(".zk.json"));
+    std::fs::File::create(zk_conf_path).and_then(|config_file| {
+        let init_config = Config { test: 5 };
+        serde_json::to_writer_pretty(config_file, &init_config)
+    })
+}
+
+fn init(zk_directory: Option<std::path::PathBuf>) -> Result<(), Error> {
+    match zk_directory {
+        Some(path) => Ok(path),
+        None => std::env::current_dir(),
+    }
+    .and_then(std::fs::canonicalize)
+    .and_then(init_zk_working_dir)
 }
 
 fn main() {
     let args = Args::parse();
-    match &args.cmd {
-        Commands::Init { zk_directory } => init(zk_directory),
-    }
+    match args.cmd {
+        Commands::Init { zk_directory } => {
+            let _init_res = init(zk_directory);
+        }
+    };
 }
